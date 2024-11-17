216aa86757a5cb55fec8dff21d8cf96873469397
MaximLebedevj <maximlebedevj@gmail.com>
2024-11-17 15:11:17 +0300

    using fetch command

--- GitSync.cmd
+++ GitSync.cmd
@@ -2,32 +2,17 @@
 
 set "repo_path=C:\Users\Maxim\OS\shared-repo"
 
-setlocal enabledelayedexpansion
-
 :loop
-set "command=git --git-dir=%repo_path%\.git remote show origin"
+git --git-dir=%repo_path%\.git fetch origin main
+for /f "delims=" %%i in ('git --git-dir=%repo_path%\.git rev-list --count HEAD...origin/main') do set status=%%i
+echo %status%
 
-set "state="
-
-for /f "tokens=* delims=" %%a in ('%command%') do (
-    set "line=%%a"
-    echo !line! | find "main pushes to main" >nul
-    if not errorlevel 1 (
-        for /f "tokens=2 delims=()" %%b in ("!line!") do (
-            set "state=%%b"
-            goto :check_state
-        )
-    )
-)
-
-:check_state
-if "%state%"=="local out of date" (
+if "%status%" NEQ "0" (
     git --git-dir=%repo_path%\.git --work-tree=%repo_path% pull origin main
 ) else (
     echo 
 )
 
-timeout /t 10 >nul
+timeout /t 10 >nul /nobreak
 
 goto :loop
-
